1f5fd34e387a0b8c8bbd97adcbb08089db3a1232
PaulineOndiek <paulineondiek76@gmail.com>
2023-06-17 13:51:59 -0700

    created the printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+*main - This is the entry point
+*return: answer is 0
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,""/n")
-return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
