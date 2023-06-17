83429aac01e5e028f1a2d72736ffb21332194ede
PaulineOndiek <paulineondiek76@gmail.com>
2023-06-17 14:07:22 -0700

    new commits

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 /*
-*main - This is the entry point
-*Retrn: 0. This shows success
+* main - This is the entry point
+* Return:Always 0 (success)
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}
