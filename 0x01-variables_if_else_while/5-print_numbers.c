f24d8f93401160115e653167fec28bd8e9e0b05c
PaulineOndiek <paulineondiek76@gmail.com>
2023-06-19 14:20:54 -0700

    created numbers with base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*Description: Creting numbers with base 10
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+{
+printf("%d", a);
+}
+printf("\n");
+return (0);
+}
