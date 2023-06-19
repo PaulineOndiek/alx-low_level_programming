2342be508a34546f67c5434ce030ce955e24893d
PaulineOndiek <paulineondiek76@gmail.com>
2023-06-19 15:04:41 -0700

    added the coma and space characters

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: Printing all numbers with single digit numbers
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a;
+for (a = 48; a <= 57; a++)
+{
+putchar(a);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
