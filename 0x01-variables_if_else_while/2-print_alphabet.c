098b44ad76f43e4a42f0dcfe2a3fd81dde7d8e39
PaulineOndiek <paulineondiek76@gmail.com>
2023-06-19 13:14:43 -0700

    created alphabets using while loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: Printing alpphabets
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int a = 97;
+while (a <= 122)
+{
+putchar(a);
+a++;
+}
+return (0);
+}
