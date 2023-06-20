297111a03bdc7d1a5ca74d0e96ca5db26a5f1d8d
PaulineOndiek <paulineondiek76@gmail.com>
2023-06-20 09:15:29 -0700

    created alphabets in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
 *main - Entry point
 *Description: printing alphabets in lowercase
-*
+*Return: Always 0 (Success)
 */
 int main(void)
 {
@@ -12,5 +12,6 @@ for (a = 97; a <= 122; a++)
 putchar(a);
 a++;
 }
+putchar('\n');
 return (0);
 }
