dacda580eb6715e7395d0fda8a47d53dc1d3f489
PaulineOndiek <paulineondiek76@gmail.com>
2023-06-22 10:35:42 -0700

    added the appropriate spaces in the code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,17 @@
 #include "main.h"
+
 /**
 *main - Test function if positive or negative
 *Description: Tests if a function is positive or negative
 *Return: Always 0 (success)
 */
+
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
