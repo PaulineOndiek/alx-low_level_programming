3f685d7bc326c9efd4cab3e51fc67bea58eb81c5
PaulineOndiek <paulineondiek76@gmail.com>
2023-06-22 05:31:20 -0700

    created a function that determines if letter is alphabet or not

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isupper - checks if input is uppercase or not
+*@c:the input
+* Return: 1 if uppercase 0 otherwise
+*/
+int _isupper(int c)
+{
+for (c = 65; c <= 90; c++)
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
