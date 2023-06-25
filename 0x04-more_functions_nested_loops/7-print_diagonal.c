2075c7a297f1b31444ae01c2726d7bfde8789694
PaulineOndiek <paulineondiek76@gmail.com>
2023-06-25 13:23:46 -0700

    crated a function for drawing a square

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,20 @@
 */
 void print_diagonal(int n)
 {
-int i;
+int a;
+int b;
 if (n <= 0)
 {
 _putchar('\n');
 }
 else
 {
-for (i = 0; i < n; i++)
+for (a = 0; a < n; a++)
 {
+for (b = 0; b < n; b++)
 _putchar(92);
 }
+_putchar(92);
 _putchar('\n');
 }
 }
