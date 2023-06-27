48dab14c6bf707eeb948581882fa155731082f0b
PaulineOndiek <paulineondiek76@gmail.com>
2023-06-27 13:03:18 -0700

    created a function that generates passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main -function that generates random passwords
+*Description: generates random passwords for
+*the program 101crackme
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int pass[100];
+int i, sum, n;
+sum = 0;
+srand(time(NULL));
+for (i = 0; i < 100; i++)
+{
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+if ((2772 - sum) -'0' < 78)
+{
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
+break;
+}
+}
+return (0);
+}
