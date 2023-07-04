0e4a41d3082e2e073ef595b75e5f062b848fabf5
PaulineOndiek <paulineondiek76@gmail.com>
2023-07-04 10:21:05 -0700

    added a description for the 0-memset file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 /**
-* _memset - fills memory with a constant value
+*_memset - fills memory with a constant value
 *@s: pointer
 *@b: value to be set
 *@n: number of bytes to be set to the value
-*return: pointer s
+*return: changed array with new value for n bytes
 */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -14,6 +14,7 @@ unsigned int i;
 for (i = 0; i < n; i++)
 {
 s[i] = b;
+n--;
 }
 return (s);
 }
