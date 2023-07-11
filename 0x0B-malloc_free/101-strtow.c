eaff4baea995c0ccdc2da2cdfdf37773e6ae8a3a
PaulineOndiek <paulineondiek76@gmail.com>
2023-07-11 13:17:02 -0700

    created the 101-strtow.c file

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+*strtow - splits a string into two words
+*@str: String
+*Return: NUmber of words
+*/
+char **strtow(char *str)
+{
+int a;
+int b;
+int c;
+}
