fd6ab74a563edc6fa60a09f5a1bf20f2cc6d4db4
PaulineOndiek <paulineondiek76@gmail.com>
2023-07-20 06:13:13 -0700

    created all the main files and .h files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+*print_name - a function that prints a name
+*@f: Second input
+*@name: Input to be printed
+*Return: Name
+*/
+void print_name(char *name, void (*f)(char *))
+{
+
+}
