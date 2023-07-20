bb5ce2e2b038df67ba154cfa612e058a97e5abd4
PaulineOndiek <paulineondiek76@gmail.com>
2023-07-20 07:07:05 -0700

    created the .h files and main files

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+/**
+*sum_them_all - a function that returns the sum of all its parameters
+*@n: integer
+*Return: Sum of parameters
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+
+}
