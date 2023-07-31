dc1f3cbfe2cf4489649effc3a7c95dbdefeb2943
PaulineOndiek <paulineondiek76@gmail.com>
2023-07-31 04:15:47 -0700

    compiled the file

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,4 +1,3 @@
-3-calc.h
 #ifndef CALC_H
 #define CALC_H
 /**
@@ -10,7 +9,8 @@ typedef struct op
 {
 char *op;
 int (*f)(int a, int b);
-} op_t;
+}
+op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
