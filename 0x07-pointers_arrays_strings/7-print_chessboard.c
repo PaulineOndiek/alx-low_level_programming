7204e29afc58600e4f3d14bacf75d91adf6af603
PaulineOndiek <paulineondiek76@gmail.com>
2023-08-01 01:17:39 -0700

    wrote a function that prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 /**
-*print_chessboard - Prints the chessboard
-*@a: array
-*Return: Always 0 (Success)
+* print_chessboard - Entry point
+* @a: array
+* Return: Always 0 (Success)
 */
 void print_chessboard(char (*a)[8])
 {
-int c;
-int b;
-for (c = 0; c < 8; c++)
+int i;
+int j;
+for (i = 0; i < 8; i++)
 {
-for (b = 0; b < 8; b++)
-{
-_putchar(a[c][b]);
+for (j = 0; j < 8; j++)
+_putchar(a[i][j]);
 _putchar('\n');
-}
 }
 }
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*print_diagsums - prints the sum of the two diagonals
-*of a square matrix of integers.
-*@a: input
-*@size: input
-*Return: Always 0 (Success)
+* print_diagsums - Entry point
+* @a: input
+* @size: input
+* Return: Always 0 (Success)
 */
 void print_diagsums(int *a, int size)
 {
-int sum1;
-int sum2;
-int i;
+int sum1, sum2, y;
 sum1 = 0;
 sum2 = 0;
-for (i = 0; i < size; i++)
+for (y = 0; y < size; y++)
 {
-sum1 = sum1 + a[i * size + i];
+sum1 = sum1 + a[y * size + y];
 }
-for (i = size - 1; i >= 0; i--)
-sum2 += a[i * size + (size - i - 1)];
+for (y = size - 1; y >= 0; y--)
+{
+sum2 += a[y * size + (size - y - 1)];
 }
 printf("%d, %d\n", sum1, sum2);
 }
