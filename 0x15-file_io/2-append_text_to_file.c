bb43cde9fa027cdb2110e703d4f15d297c0b3ba2
PaulineOndiek <paulineondiek76@gmail.com>
2023-08-07 12:26:15 -0700

    created a function that creates a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+/**
+*append_text_to_file - appends text at the end of a file
+*@filename: pointer
+*@text_content: string to add to the end of file
+*Return: -1 or 1
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+}
